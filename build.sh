5841e08701eabb174649680397633aff50ae795b
deromask <git@deromask.io>
2020-02-16 14:25:46 +0700

    update build script

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ flutter build appbundle --release
 #flutter build appbundle --release --target-platform=android-arm64
 #flutter build appbundle --release --target-platform=android-x86
 #flutter build appbundle --release --target-platform=android-x64
+#flutter build apk --target-platform android-arm,android-arm64,android-x64 --split-per-abi
